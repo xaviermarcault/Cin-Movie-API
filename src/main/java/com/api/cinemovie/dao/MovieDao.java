aa79e23657d1f12b14b9e58469df98296cedfcf3
marcault xavier <xavier.marcault@gmail.com>
2021-05-30 18:15:50 +0200

    ajout fonction tri avec limite plus grand que année

--- src/main/java/com/api/cinemovie/dao/MovieDao.java
+++ src/main/java/com/api/cinemovie/dao/MovieDao.java
@@ -1,12 +1,15 @@
 package com.api.cinemovie.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.api.cinemovie.model.Movie;
 
 @Repository
-public interface MovieDao extends JpaRepository<Movie,Integer>{
+public interface MovieDao extends JpaRepository<Movie, Integer> {
 
 	public Movie findById(int id);
+	List<Movie> findByYearGreaterThan(int yearLimit);
 }
