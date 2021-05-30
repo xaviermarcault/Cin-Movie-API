9b516ee9bd8170d7020699dc6ce2130c16433cd2
marcault xavier <xavier.marcault@gmail.com>
2021-05-30 17:55:54 +0200

    Ajout léthode PUT

--- src/main/java/com/api/cinemovie/controller/MovieController.java
+++ src/main/java/com/api/cinemovie/controller/MovieController.java
@@ -2,12 +2,14 @@ package com.api.cinemovie.controller;
 
 import java.net.URI;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -37,17 +39,20 @@ public class MovieController {
 	@PostMapping(value = "/films")
 	public ResponseEntity<Void> ajouterFilm(@RequestBody Movie movie) {
 		Movie movieAdded = movieDao.save(movie);
-		
+
 		if (movieAdded == null) {
 			ResponseEntity.noContent().build();
 		}
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(movie.getId())
-				.toUri();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(movie.getId()).toUri();
 
 		return ResponseEntity.created(location).build();
+	}
+
+	// Modifier un film
+	@PutMapping(value = "/films")
+	public void modifierUnFilm(@RequestBody Movie movie) {
+		movieDao.save(movie);
 	}
 
 	// Supprimer un film
